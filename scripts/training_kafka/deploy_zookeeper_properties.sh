94e7cf20c955dff77489dc8896aeb7bedfe786eb
Karthik N <ext_nkarthik@falabella.cl>
2021-03-05 14:20:51 +0530

    Story#8 |Nitika, Karthik| - add deployment step to update configs of zookeeper and update existing deployment step to add ksfks config for multiple nodes

--- scripts/training_kafka/deploy_zookeeper_properties.sh
+++ scripts/training_kafka/deploy_zookeeper_properties.sh
@@ -39,11 +39,12 @@ Host kafka3.${TRAINING_COHORT}.training
 echo "====SSH Config Updated===="
 for host in kafka.${TRAINING_COHORT}.training kafka2.${TRAINING_COHORT}.training kafka3.${TRAINING_COHORT}.training
 do
-  scp ./images/training_kafka/conf/server.properties ec2-user@${host}:/tmp/kafka-server.properties
+  scp ./images/training_kafka/conf/zookeeper.properties ec2-user@${host}:/tmp/zookeeper.properties
   ssh ${host} <<EOF
   set -e
-  sudo mv /tmp/kafka-server.properties /etc/kafka/server.properties
-  sudo systemctl restart confluent-kafka
+  sudo mv /tmp/zookeeper.properties /etc/kafka/zookeeper.properties
+  sudo systemctl stop confluent-kafka
+  sudo systemctl restart confluent-zookeeper
 EOF
 done
-echo "=== Kafka server properties deployed successfully"
+echo "=== Zookeeper properties deployed successfully"
